65a7f3ff60619faff7705107f1a095d73d61cba7
Rohan <rohan@localhost.localdomain>
2018-02-12 15:28:44 -0500

    Added the MState function. Added program checks for null commands

--- Interpreter Pt 1/interpret.scm
+++ Interpreter Pt 1/interpret.scm
@@ -1,3 +1,4 @@
+#lang racket
 ; Interpreter Pt 1 EECS 345
 ; Group Members: Lee Radics, Zach Perlo, Rohan Krishna
 ; Case IDs:elr61, zip5, rxr353
@@ -7,8 +8,35 @@
 ;The interpreter function used to parse/interpret the java-like file (.javal)
 (define interpret
   (lambda (filename)
-    (evaluate filename)
-    (M_state (parser filename) S) ; where S is the base state (probably blank list, will determine later)
+    (evaluate (parser filename) '(()()));passes the filename to evaluate/parse program tree
+    )
   )
-)
+  
 
+;Function to evaluate and step through program tree
+(define evaluate
+  (lambda (prgm state)
+    (cond
+      ((and (null? prgm) (eq? state #t)) "true");if prgm null and equals state true
+      ((and (null? prgm) (eq? state #f)) "false")
+      ((null? prgm) state);now finally check if prgm is null, then return state
+      (else (evaluate (cdr prgm) (MState (car prgm) state)));evaluate filename while calling MState on the command
+      )                                                     ;if checks are passed 
+    )
+  )
+
+;Function to return program state after command is run
+;Commands: var, =, if, while, return and the state is ((x y ...) (1 2 ...))
+(define MState
+  (lambda (command state)
+    (cond
+      ((eq? state "error") "error")
+      ((eq? (car command) 'var) (declareHelper command state))
+      ((eq? (car command) '=) (assignHelper command state))
+      ((eq? (car command) 'if) (ifHelper command state))
+      ((eq? (car command) 'while) (whileHelper command state))
+      ((eq? (car command) 'return) (returnHelper command state))
+      (else "error")
+      )
+    )
+  )
