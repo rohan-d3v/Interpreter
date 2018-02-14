16c9be58fd4768f718b56e1c71676f63c2b10928
Rohan <rohan@localhost.localdomain>
2018-02-14 15:41:14 -0500

    Added extra help methods for lookup and expression check

--- Interpreter Pt 1/interpret.scm
+++ Interpreter Pt 1/interpret.scm
@@ -1,4 +1,4 @@
-;#lang racket
+; #lang racket
 ; Interpreter Pt 1 EECS 345
 ; Group Members: Lee Radics, Zach Perlo, Rohan Krishna
 ; Case IDs:elr61, zip5, rxr353
@@ -297,3 +297,75 @@
 ;======================;
 ;Other Helper Methods
 ;======================;
+; returns #t if the variable being assigned has not been declared
+; takes assignment command (= z 6) and state-vars (x y z ...) aka (car state)
+(define firstVarNotDeclared
+  (lambda (command state-vars)
+    (cond
+      ((null? state-vars)
+       #t);null state
+      ((eq? (car state-vars) (car(cdr command)))
+       #f);returns #f if variable is declared
+      (else (firstVarNotDeclared command (cdr state-vars)))
+      )
+    )
+  )
+
+; returns #t if the variable has been declared (is in the state)
+(define isInList
+  (lambda (var state)
+    (cond
+      ((null? state)
+       #f);null state
+      ((null? (car state))
+       #f);expression null
+      ((eq? var (car (car state)))
+       #t);true if declared
+      (else (isInList var (cons (cdr (car state)) (cons (cdr (car (cdr state))) '()))))
+      )
+    )
+  )
+
+; Adds the variable and value
+; takes command (var x expr) (expr optional) and state ((z y ...) (1 2 ...))
+(define addToList
+  (lambda (var state)
+    (cond
+      (cond
+      ((null? (cdr (cdr command))); to check if expressions has been passed
+       (cons (cons (car (cdr command)) (car state)) (cons (cons "undefined" (car (cdr state))) '())))
+      ((eq? (MValue (cons (car (cdr (cdr command))) '()) state) "error");if the state is an error
+       (cons (cons (car (cdr command)) (car state)) (cons (cons (MBoolean (cons (car (cdr (cdr command))) '()) state) (car (cdr state))) '())))
+      (else (cons (cons (car (cdr command)) (car state)) (cons (cons (MValue (cons (car (cdr (cdr command))) '()) state) (car (cdr state))) '())))
+      )
+      )
+    )
+  )
+
+; updates the state with the variable-value pair
+; takes command (x expr) and state ((x y ...) (1 2 ...))
+; this is a contingency method. You should never have to use this
+(define updateList
+  (lambda (command state)
+    (cond
+      ((null? (car state))
+       #f) ; Should never get here because we already checked to see if variable can be added to list
+      ((eq? (car command) (car (car state)))
+       (cons (MValue (cons (car (cdr command)) '()) state) (cdr (car (cdr state)))))
+      (else (cons (car (car (cdr state))) (updateList command (cons (cdr (car state)) (cons (cdr (car (cdr state))) '())))))
+      )
+    )
+  )
+; Returns the value of the variable as stored in the state
+; takes var as a variable name and state ((x y ...) (1 2 ...))
+(define lookup
+  (lambda (var state)
+    (cond
+      ((null? (car state))
+       "error");returns error if x isn't in the state
+      ((eq? (car (car state)) var)
+       (car (car (cdr state))));standard find method
+      (else (lookup var (cons (cdr (car state)) (cons (cdr (car (cdr state))) '()))))
+      )
+    )
+  )
